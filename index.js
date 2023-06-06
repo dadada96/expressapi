f6c03bee4cfc9ca427993e4d4e9fd20195848bee
Saifullah <saifullah@Saifullahs-MacBook-Air.local>
2023-06-06 13:40:52 +0800

    meteomatic

--- index.js
+++ index.js
@@ -14,10 +14,24 @@ app.get('/users', (req, res) => {
     //     age: '27'
     // })
 
-    request('https://jsonplaceholder.typicode.com/posts', function(error, response, body) {
-        console.log(body)
-        res.status(200).send(body)
-    })
+    // request('https://jsonplaceholder.typicode.com/posts', function(error, response, body) {
+    //     console.log(body)
+    //     res.status(200).send(body)
+    // })
+
+    var request = require('request');
+    var options = {
+        'method': 'GET',
+        'url': 'https://login.meteomatics.com/api/v1/token',
+        'headers': {
+            'Authorization': 'Basic YWVyb2R5bmVncm91cF9kZXY6djlyNVN0SkI5Mg=='
+        }
+    };
+    request(options, function (error, response) {
+        if (error) throw new Error(error);
+        console.log(response.body);
+    });
+
 })
 
 module.exports = app;
