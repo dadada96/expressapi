884bf76acc86efc85b7c26ba4e1db458f5031d6e
Saifullah <saifullah@Saifullahs-MacBook-Air.local>
2023-06-06 13:29:09 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+const PORT = 2000;
+
+const request = require('request')
+
+app.listen(
+    PORT,
+    () => console.log(`its live on port ${PORT} !!!`)
+)
+
+app.get('/users', (req, res) => {
+    // res.status(200).send({
+    //     name: 'ahmad',
+    //     age: '27'
+    // })
+
+    request('https://jsonplaceholder.typicode.com/posts', function(error, response, body) {
+        console.log(body)
+        res.status(200).send(body)
+    })
+})
